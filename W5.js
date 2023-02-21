0e610389bd5d3ac9dc21e7442b22b1c94742d664
Cole Yeager <106110474+ColeYeager19@users.noreply.github.com>
2023-02-20 22:24:13 -0700

    Update W5.js

--- W5.js
+++ W5.js
@@ -1,10 +1,7 @@
 // JavaScript Document
 
-// create an array of image names that correspond to the image tags
 var imageTags = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9", "image10", "image11", "image12", "image13", "image14", "image15", "image16", "image17", "image18", "image19", "image20","image21", "image22", "image23", "image24"];
-// create a variable with the blank image name
 var blankImagePath = "images/Blank.jpg";
-// create a empty array for the actual images
 var actualImages = new Array();
     
 function printBlanks()
@@ -19,20 +16,14 @@ function printBlanks()
 
 function createRandomImageArray()
 {
-    // create an array of actual images	
 	var actualImagePath = ["MountainDog.jpg", "Husky.jpg", "BlackLab.jpg", "CockerSpaniel.jpg", "GermanShepard.jpg", "KingCharles.jpg", "GoldenRetriever.jpeg", "ChocolateLab.jpg", "Boz.jpg", "BorderCollie.jpg", "Akita.jpg", "Raccoon.jpg"];
 	var count = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-    // create a while statement to check to see if our actual image array is full
     while(actualImages.length < 24)
     {
-        // get a random number between 0 and the number total number of images that we can choose from
         var randomNumber = Math.floor(Math.random() * actualImagePath.length)
-          // create an if statement that says if the total number added is less than 2, then
-        // add the image to the actual image array
         if(count[randomNumber] < 2)
         {
             actualImages.push(actualImagePath[randomNumber]);
-            // then add one to the array that makes sure only two images can be added
             count[randomNumber] = count[randomNumber] + 1;
         }
     }
